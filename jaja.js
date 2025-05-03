b6d4f6d739bc20e0b87ae1618300cdc64a956c4e
polina tashireva <152691466+knif-blood@users.noreply.github.com>
2025-05-03 21:15:35 +0800

    Update jaja.js

--- jaja.js
+++ jaja.js
@@ -1,15 +1,15 @@
 
 let sign = prompt("What's your sign?");
 
-if (sign.toLowerCase() === "scorpio") {
-  alert("Wow! I'm a Scorpio too!");
+if (sign.toLowerCase() === "Aries""Taurus""Gemini""Cancer""Leo""Virgo""Libra""scorpio""Sagittarius""Capricorn""Aquarius""Pisces") {
+  alert("Wow! I'm a Scorpio too!""Wow! I'm a Aries too!""Wow! I'm a Taurus too!""Wow! I'm a Gemini too!""Wow! I'm a Cancer too!""Wow! I'm a Leo too!""Wow! I'm a Virgo too!""Wow! I'm a Libra too!""Wow! I'm a Sagittarius too!""Wow! I'm a Capricorn too!""Wow! I'm a Aquarius too!""Wow! I'm a Pisces too!");
 }
 
 // there are many ways to use the prompt feature
 sign = window.prompt(); // open the blank prompt window
 sign = prompt(); //  open the blank prompt window
 sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
-sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
+sign = window.prompt("What is you name?", "Polina""Elena""George""Nastya""Alice""Timur""Eva""Marianna""Natalya""Sveta""Oleg"); // open the window with Text "Are you feeling lucky" and default value "Polina""Elena""George""Nastya""Alice""Timur""Eva""Marianna""Natalya""Sveta""Oleg"
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
